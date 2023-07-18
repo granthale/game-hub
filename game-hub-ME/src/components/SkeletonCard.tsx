c5effefe2d32667038a26d91736d8f3a404e3dd4
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-07-18 14:40:04 -0700

    S

--- game-hub-ME/src/components/SkeletonCard.tsx
+++ game-hub-ME/src/components/SkeletonCard.tsx
@@ -1,5 +1,7 @@
 import { Card, CardBody, Skeleton, SkeletonText } from "@chakra-ui/react";
 
+
+
 const SkeletonCard = () => {
   return (
     <Card borderRadius={10} overflow="hidden" width="300px">
