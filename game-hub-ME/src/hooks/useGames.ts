b8986d5df1ea040ca9744d5e043b76f83c8f0ec4
Grant Hale <64755492+granthale@users.noreply.github.com>
2023-07-19 12:33:45 -0700

    Refactored platform & genre into gameQuery object

--- game-hub-ME/src/hooks/useGames.ts
+++ game-hub-ME/src/hooks/useGames.ts
@@ -1,3 +1,4 @@
+import { GameQuery } from "../App";
 import useData from "./useData";
 import { Genre } from "./useGenres";
 
@@ -15,9 +16,8 @@ export interface Game {
     metacritic: number;
 }
 
-
-const useGames = (selectedGenre: Genre | null, selectedPlatform : Platform | null) => {
-    const {data:games, error, isLoading} = useData<Game>('/games', {params: {genres: selectedGenre?.id, parent_platforms: selectedPlatform?.id}}, [selectedGenre?.id, selectedPlatform?.id])
+const useGames = (gameQuery : GameQuery) => {
+    const {data:games, error, isLoading} = useData<Game>('/games', {params: {genres: gameQuery.genre?.id, parent_platforms: gameQuery.platform?.id}}, [gameQuery])
 
     return {games, error, isLoading};
 }
